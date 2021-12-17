4fb7acc706b03358628292ed0dcc25b0f97c2925
Rishan Annadurai <annadurai.r@northeastern.edu>
2021-12-17 10:21:23 -0500

    post-demo commit

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,2 +1,3 @@
 export const API_KEY = "BQAZptwhCKgEdXmx1uTEehNEQ7OTRofbk9_9l_cO6Br7jVKbkGMkTf4yEVwpJIT-rVHacuCs0jORzs3vR6k"
+// API_KEY needs to be regenerated every 1 hour as per Spotify regulations... please ping me if you need it
 export const API_URL = "http://localhost:4000/api"
